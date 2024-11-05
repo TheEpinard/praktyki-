e9aa625109a2595fb736dd4f11ff3ba6aa16d7fc
Michał <mkolny02@gmail.com>
2024-11-06 00:08:25 +0100

    Add files via upload

---
+++ results.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="style.css">
+<?php
+require 'db_connect.php';
+
+
+$sql = "SELECT * FROM results ORDER BY date DESC"; 
+$stmt = $pdo->query($sql);
+
+
+echo "<h2>Historia Gier</h2>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Data</th><th>Twoje Liczby</th><th>Wylosowane Liczby</th><th>Trafienia</th><th>Wygrana</th></tr>";
+
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . $row['date'] . "</td>";
+    echo "<td>" . $row['user_numbers'] . "</td>";
+    echo "<td>" . $row['random_numbers'] . "</td>";
+    echo "<td>" . $row['matches'] . "</td>";
+    echo "<td>" . $row['prize'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+echo '<br><form action="index.php" method="get">
+        <button type="submit">Wróć</button>
+      </form>';
+?>
